ba5156349ce29c95c46f135a72ee72b501637923
Dhirachat Chayaporn <omiejung@gmail.com>
2026-09-13 11:42:58 +0700

    My Pill Reminder - 01

---
+++ MyPillReminder/MyPillReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPillReminderApp.swift
+//  MyPillReminder
+//
+//  Created by Omie C on 13/9/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct MyPillReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Omega_Coin
+This is for record and remind the patient to take a pill regularly. 01
+.
+# todo
+Profile picture
+
+
